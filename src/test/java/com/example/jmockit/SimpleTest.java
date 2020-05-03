a1bc8950441a6aea84ce4e88ab987e608e8f53e6
tadashi izuno <tadashi.izuno@gmail.com>
2020-05-03 11:13:02 +0900

    maven 設定変更

--- src/test/java/com/example/jmockit/SimpleTest.java
+++ src/test/java/com/example/jmockit/SimpleTest.java
@@ -4,11 +4,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
+import mockit.Mock;
+import mockit.MockUp;
 
 public class SimpleTest {
 
     @Test
     public void test() {
+        new MockUp<java.lang.Math>() {
+            @Mock
+            public double random() {
+                // 常に2.5を返すrandom()メソッド
+                return 2.5;
+            }
+        };
         assertEquals(2.5, Math.random(), 0.1);
         assertEquals(2.5, Math.random(), 0.1);
     }
